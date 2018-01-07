67e918a2cff39b2ca6440751d92f7ad31be31a9e
Gautam Sharma <gautamsharma7234@gmail.com>
2018-01-07 23:51:46 +0530

    conversion script upgraded

--- mkv2mp4.py
+++ mkv2mp4.py
@@ -7,4 +7,5 @@ import os
 
 for i,f in enumerate(glob.glob('*.mkv')):
     filename = f.strip('.mkv')
-    os.system('ffmpeg -i "{0}".mkv -c:v libx264 -c:a aac  "{0}".mp4'.format(filename))
+    #os.system('ffmpeg -i "{0}".mkv -c:v libx264 -c:a aac  "{0}".mp4'.format(filename))
+    os.system('ffmpeg -i "{0}".mkv -strict experimental -c:v copy -c:a aac -b:a 192k "{0}".mp4'.format(filename))
